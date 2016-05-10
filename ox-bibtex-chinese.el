f697a76f6626ae4c3a34aedd90ad1bceb42ec992
Syohei YOSHIDA <syohex@gmail.com>
2016-05-10 11:09:56 +0900

    Add org-plus-contrib dependency for using ox-bibtex

--- ox-bibtex-chinese.el
+++ ox-bibtex-chinese.el
@@ -6,7 +6,7 @@
 ;; Author: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/ox-bibtex-chinese.git
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "24.4") (org-plus-contrib "20150504"))
 
 ;; This file is not part of GNU Emacs.
 
